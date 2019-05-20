a00944df0f094914778590bb4c7ca64fcb6e4f6e
C4rist0p4 <Christoph_S@posteo.de>
2019-05-20 19:30:52 +0200

    20.05.2019

--- buildhr/spiders/buildhr_emails.py
+++ buildhr/spiders/buildhr_emails.py
@@ -10,7 +10,6 @@ import numpy as np
 from validate_email import validate_email
 from imageio import imread
 from cv2 import cv2
-from PIL import Image
 from scrapy_splash import SplashRequest
 
 script = '''
@@ -54,58 +53,62 @@ end
 
 
 class BuildhrSpiderSpider(scrapy.Spider):
-  name = 'buildhr_emails'
-    
-  def start_requests(self):
-    
-    file_path = os.path.join(os.path.dirname(__file__),'missed_email_10.csv')
+    name = 'buildhr_emails'
 
-    with open(file_path, 'r', encoding='utf8') as in_file:
-        csv_reader = csv.DictReader(in_file)
+    def start_requests(self):
 
-        for url in csv_reader:
-            self.logger.info(url['Orignal Page URL'])
-            yield SplashRequest(url=url['Orignal Page URL'], callback=self.parse_img, endpoint='execute', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}, args={'wait': 3, 'lua_source': script})
+        file_path = os.path.join(os.path.dirname(
+            __file__), 'missed_email_10.csv')
 
-  def parse_img(self, response):
-  
-    data = json.loads(response.body_as_unicode())
+        with open(file_path, 'r', encoding='utf8') as in_file:
+            csv_reader = csv.DictReader(in_file)
 
-    if data['jpeg'] != False:
+            for url in csv_reader:
+                self.logger.info(url['Orignal Page URL'])
+                yield SplashRequest(url=url['Orignal Page URL'], callback=self.parse_img, endpoint='execute', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}, args={'wait': 3, 'lua_source': script})
 
-        stream = imread(io.BytesIO(base64.b64decode(data['jpeg'])))
-        
-        email = self.img_to_text(stream)
-        is_valid = validate_email(email)
+    def parse_img(self, response):
 
-        if is_valid:
-          yield {
-          'Orignal Page URL': data['OrignalURL'],
-          'EMail': email
-          }
+        data = json.loads(response.body_as_unicode())
+
+        if data['jpeg'] != False:
+
+            stream = imread(io.BytesIO(base64.b64decode(data['jpeg'])))
+
+            email = self.img_to_text(stream)
+            is_valid = validate_email(email)
+
+            if is_valid:
+                yield {
+                    'Orignal Page URL': data['OrignalURL'],
+                    'EMail': email
+                }
+            else:
+                self.logger.info('Email ' + email)
+                yield SplashRequest(url=data['OrignalURL'], callback=self.parse_img, endpoint='execute', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}, args={'wait': 3, 'lua_source': script}, dont_filter=True)
         else:
-          self.logger.info('Email '+ email)
-          yield SplashRequest(url=data['OrignalURL'], callback=self.parse_img, endpoint='execute', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}, args={'wait': 3, 'lua_source': script}, dont_filter = True)
-    else:
-      yield {
-          'Orignal Page URL': data['OrignalURL'],
-          'EMail': 'NO Email'
-      }
+            yield {
+                'Orignal Page URL': data['OrignalURL'],
+                'EMail': 'NO Email'
+            }
 
-  def img_to_text(self, stream):
+    def img_to_text(self, stream):
 
-    img = cv2.cvtColor(stream, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(stream, cv2.COLOR_BGR2GRAY)
 
-    img = cv2.resize(img, None, fx=15, fy=15, interpolation=cv2.INTER_CUBIC)
+        img = cv2.resize(img, None, fx=10, fy=10,
+                         interpolation=cv2.INTER_CUBIC)
 
-    kernel = np.ones((1, 1), np.uint8)
+        kernel = np.ones((1, 1), np.uint8)
 
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
+        img = cv2.dilate(img, kernel, iterations=1)
+        img = cv2.erode(img, kernel, iterations=1)
 
-    img = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        img = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0),
+                            0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-    text = pytesseract.image_to_string(img, config='--psm 11')
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
-    return text
-           
+        text = pytesseract.image_to_string(img, config='--psm 11')
+
+        return text
--- buildhr/spiders/buildhr_spider.py
+++ buildhr/spiders/buildhr_spider.py
@@ -5,8 +5,13 @@ import io
 import base64
 import json
 import csv
+import os
+import numpy as np
 from scrapy_splash import SplashRequest
 from PIL import Image
+from validate_email import validate_email
+from imageio import imread
+from cv2 import cv2
 
 script = '''
 function isempty(s)
@@ -16,25 +21,31 @@ end
 function main(splash, args)
 
   assert(splash:go(splash.args.url))
-  assert(splash:wait(0.5))
+  assert(splash:wait(12))
 
   splash:set_viewport_full()
 
-  local get_img = splash:jsfunc([[
-    function (element) {
-      var el = element.parentElement;
-      var r = el.getBoundingClientRect();
-      return [r.left+8, r.top, r.right, r.bottom];
+  local get_bbox = splash:jsfunc([[
+    function(element, search_parameter) {
+			var elements_p = document.getElementsByClassName('tits');
+
+    	var element_coordinates = false;
+    	var i;
+      for (i = 0; i < elements_p.length; i++) {
+        if (elements_p[i].textContent == search_parameter) {
+    			 	var el = elements_p[i].nextElementSibling;
+    				var r = el.getBoundingClientRect();
+         		element_coordinates = [r.left+8, r.top, r.right, r.bottom];
+  			}
+      }
+
+    	if (element_coordinates != false) {
+    		return element_coordinates
+  		} else {
+    		return false
+  		}
     }
   ]])
-
-  local elements = splash:select_all("ul.contact > li > p.cons > img")
-	local img = {}
-
-  for i, element in ipairs(elements) do
-       local region = get_img(element)
-       img[i] = splash:jpeg{region=region}
-  end
 
   local companyname = splash:select("div.details-of-right > h2")
   local website = splash:select("ul.contact > li > p.cons  > a")
@@ -51,23 +62,41 @@ function main(splash, args)
       companyname = companyname:text()
   end
 
-  return {jpeg=img,CompanyName=companyname,OrignalURL=splash.args.url, Website=website}
+  local img_email = false
+  local region = get_bbox(elem, "邮箱")
+  if (region ~= false) then
+    img_email = splash:jpeg{region=region}
+  end
+
+  local img_number = false
+  region = get_bbox(elem, "座机")
+  if (region ~= false) then
+    img_number = splash:jpeg{region=region}
+  end
+
+
+  return {ImgEmail=img_email,ImgNumber=img_number, CompanyName=companyname,OrignalURL=splash.args.url, Website=website}
 end
 '''
 
 
 class BuildhrSpiderSpider(scrapy.Spider):
     name = 'buildhr_spider'
-    #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
     def start_requests(self):
-        with open(r'C:\Users\Admin\Projekte\Buildhr-master\buildhr\spiders\URLS.csv', 'r', encoding='utf8') as in_file:
+
+        file_path = os.path.join(os.path.dirname(
+            __file__), 'missed_email_10.csv')
+
+        with open(file_path, 'r', encoding='utf8') as in_file:
             csv_reader = csv.DictReader(in_file)
 
             for url in csv_reader:
-                u = url['URL']
+                u = url['Orignal Page URL']
                 self.logger.info(u)
-                yield scrapy.Request(url=u, callback=self.parse)
+                # yield scrapy.Request(url=u, callback=self.parse)
+                yield SplashRequest(url=u, callback=self.parse_img, endpoint='execute', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}, args={'wait': 3, 'lua_source': script})
 
     def parse(self, response):
         links = response.xpath('.//a/@href').getall()
@@ -78,23 +107,61 @@ class BuildhrSpiderSpider(scrapy.Spider):
                 yield SplashRequest(url=link, callback=self.parse_img, endpoint='execute', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}, args={'wait': 3, 'lua_source': script})
 
     def parse_img(self, response):
-        email = []
-        number = []
+        email = None
+        number = None
 
         data = json.loads(response.body_as_unicode())
 
-        for i in data['jpeg']:
-            stream = io.BytesIO(base64.b64decode(data['jpeg'][i]))
-            text = pytesseract.image_to_string(Image.open(stream))
-            if "@" in text or '.com' in text:
-                email.append(text)
-            else:
-                number.append(text)
+        if data['ImgNumber'] != False:
+            stream = imread(io.BytesIO(base64.b64decode(data['ImgNumber'])))
+            number = self.img_to_text(stream)
 
-        yield {
-            'Orignal Page URL': data['OrignalURL'],
-            'Company Name': data['CompanyName'],
-            'Phone Number': number,
-            'Website': data['Website'],
-            'EMail': email
-        }
+        if data['ImgEmail'] != False:
+
+            stream = imread(io.BytesIO(base64.b64decode(data['ImgEmail'])))
+
+            email = self.img_to_text(stream)
+            is_valid = validate_email(email)
+
+            if is_valid:
+                yield {
+                    'Orignal Page URL': data['OrignalURL'],
+                    'Company Name': data['CompanyName'],
+                    'Phone Number': number,
+                    'Website': data['Website'],
+                    'EMail': email
+                }
+
+            else:
+                self.logger.info('Email ' + email)
+                yield SplashRequest(url=data['OrignalURL'], callback=self.parse_img, endpoint='execute', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}, args={'wait': 3, 'lua_source': script}, dont_filter=True)
+
+        else:
+            yield {
+                'Orignal Page URL': data['OrignalURL'],
+                'Company Name': data['CompanyName'],
+                'Phone Number': number,
+                'Website': data['Website'],
+                'EMail': email
+            }
+
+    def img_to_text(self, stream):
+
+        img = cv2.cvtColor(stream, cv2.COLOR_BGR2GRAY)
+
+        img = cv2.resize(img, None, fx=10, fy=10,
+                         interpolation=cv2.INTER_CUBIC)
+
+        kernel = np.ones((1, 1), np.uint8)
+
+        img = cv2.dilate(img, kernel, iterations=1)
+        img = cv2.erode(img, kernel, iterations=1)
+
+        img = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0),
+                            0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+        text = pytesseract.image_to_string(img, config='--psm 11')
+
+        return text
